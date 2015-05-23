9e3b234fd95eb2c221b37bcfd3a531dfddff6d3c
Arnab Ghosh <arnabgho@cse.iitk.ac.in>
2015-05-23 23:24:08 +0530

    Recommendation module

---
+++ server/recommend.py
@@ -0,0 +1,88 @@
+import os
+import pandas as pd
+import numpy as np
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import Imputer
+import pickle
+import csv
+from sklearn import cross_validation
+from sklearn.metrics import accuracy_score
+import math
+import numpy
+import random
+
+users = 10
+pos = 24
+ads = 50
+K=2
+
+with open("L-file.pickle",'r') as f:
+	L=pickle.load(f)
+
+def getNorm(X):
+	ans=0
+	for x in X:
+		ans+=x*x
+	return math.sqrt(ans)	
+
+def getCosineSimilarity(a,b):
+	cosines=[]
+	for j in xrange(0,pos):
+		temp=0
+		x=[]
+		y=[]
+		for k in xrange(0,ads):
+			temp+=L[a][j][k]*L[b][j][k]
+			x.append(L[a][j][k])
+			y.append(L[b][j][k])
+		xnorm=getNorm(x)
+		ynorm=getNorm(y)
+		if(xnorm==0  or ynorm==0):
+			cosines.append(0)
+		else:
+			cosines.append(temp/(xnorm*ynorm))
+	return getNorm(cosines)
+	
+def getRecommendation(a):
+	IDS=[]
+	SIM=[]
+	for i in xrange(0,users):
+		if(i==a):
+			continue
+		IDS.append(i)
+		SIM.append(getCosineSimilarity(a,i))
+
+	allPairs=zip(SIM,IDS)
+	s=sorted(allPairs,reverse=True)
+
+	res=[0]*ads
+	# allres=[0]*pos
+	
+	for k in xrange(0,ads):
+		temp=[0]*pos
+		for j in xrange(0,pos):
+			for i in xrange(0,K):
+				ID=allPairs[i][1]
+				temp[j]+=L[ID][j][k]
+			temp[j]/=2
+		res[k]=getNorm(temp)		
+	return res			
+
+
+def personChecking(a,state):
+	ad_reco=getRecommendation(a)
+	IDS=[]
+	for i in xrange(0,ads):
+		IDS.append(i+1)
+	P=zip(ad_reco,IDS)	
+	return sorted(P,reverse=True)
+
+
+def main():
+	print "Enter Person "
+	a=int(raw_input())
+	state=int(raw_input())
+	print personChecking(a,state)	
+
+main()
