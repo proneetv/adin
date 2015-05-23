33df164d4b2220ca856d55ab2a92b0d0f385e498
Triveni Mahatha <triveni692@gmail.com>
2015-05-23 19:41:39 +0530

    Sample data, actual values

---
+++ data.py
@@ -0,0 +1,42 @@
+import math
+import numpy
+import random
+
+mu = 3
+stdDev = 2
+samples = 1000
+
+users = 10
+pos = 24
+ads = 50
+
+L = []
+Ltmp = []
+for i in xrange (0, users):
+	L1 = []
+	for p in xrange(0, pos):
+		L2 = []
+		cnt = 0
+		Ltmp = numpy.random.normal(mu, stdDev, samples)
+		for xx in Ltmp:
+			if (xx >= 0 and xx <= 6):
+				cnt += 1
+				L2.append(int(xx))
+			if cnt == ads:
+				break
+		assert(len(L2) == ads)
+		random.shuffle(L2)
+		L1.append(L2)
+	assert(len(L1) == pos)
+	L.append(L1)
+
+
+while (True):
+	i, j, k = map(int, raw_input().split())
+	
+	assert(0 <= i and i < users)
+	assert(0 <= j and j < pos)
+	assert(0 <= k and k < ads)
+	print L[i][j][k]
+
+# print L
